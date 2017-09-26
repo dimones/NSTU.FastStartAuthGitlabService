408b5f9d591645c421eef500693f926f5ed35396
Дмитрий Богомолов <Dimones951@yandex.ru>
2017-09-26 12:59:07 +0700

    Repeat auth

--- API.py
+++ API.py
@@ -97,6 +97,9 @@ class Gitlab:
     def get_users(self):
         print('get users')
         print('token ' + self.root_gitlab_token)
+        while self.root_gitlab_token == None:
+            self.root_gitlab_token = self.auth_gitlab('root','jmXQF97J')
+            print('fucking repeat auth')
         r = requests.get(server_address  + 'users', headers={"PRIVATE-TOKEN": self.root_gitlab_token})
         return r.json()
 
